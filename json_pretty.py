161aa1c6e087b413995683c1e7cabad5258dbee9
Nicole B <nicole@faenre.dev>
2021-03-11 09:57:27 -0500

    added status printing

--- json_pretty.py
+++ json_pretty.py
@@ -4,20 +4,26 @@ import sys
 file_name = sys.argv[1]
 
 if (file_name[-5:] == ".json") :
-    # read ugly json data
-    with open(file_name, 'r') as file_reader :
-        json_content = json.load(file_reader)
+    try :
+        # read ugly json data
+        with open(file_name, 'r') as file_reader :
+            json_content = json.load(file_reader)
 
-    # format the text into a pretty string
-    formatted_text = json.dumps(
-        json_content,
-        indent = 2,
-        separators = (',', ': '),
-        sort_keys = True,
-    )
+        # format the text into a pretty string
+        formatted_text = json.dumps(
+            json_content,
+            indent = 2,
+            separators = (',', ': '),
+            sort_keys = True,
+        )
 
-    # overwrite ugly text with prettified text
-    with open(file_name, 'w') as file_writer :
-        file_writer.write(formatted_text)
+        # overwrite ugly text with prettified text
+        with open(file_name, 'w') as file_writer :
+            file_writer.write(formatted_text)
+    except :
+        print('Error found. Traceback:')
+        traceback.print_exc()
+    else:
+        print('Success')
 else :
     print("error: not .json")
