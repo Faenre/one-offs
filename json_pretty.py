402395f85e958d06d9aba2bd33c4bcd3eba2c5df
Nicole B <nicole@faenre.dev>
2021-03-11 09:48:14 -0500

    formats js rapidly

---
+++ json_pretty.py
@@ -0,0 +1,23 @@
+import json
+import sys
+
+file_name = sys.argv[1]
+
+if (file_name[-5:] == ".json") :
+    # read ugly json data
+    with open(file_name, 'r') as file_reader :
+        json_content = json.load(file_reader)
+
+    # format the text into a pretty string
+    formatted_text = json.dumps(
+        json_content,
+        indent = 2,
+        separators = (',', ': '),
+        sort_keys = True,
+    )
+
+    # overwrite ugly text with prettified text
+    with open(file_name, 'w') as file_writer :
+        file_writer.write(formatted_text)
+else :
+    print("error: not .json")
